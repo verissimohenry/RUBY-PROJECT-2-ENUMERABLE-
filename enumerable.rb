64dfb1c2e869c3aaa82ab82373a5e331b20c2578
verissimohenry <verissimohenry04@gmail.com>
2021-04-02 18:21:32 +0100

    solution each

--- enumerable.rb
+++ enumerable.rb
@@ -1,12 +1,12 @@
 module Enumerable
-      def my_each
-        a = 0
-        return to_enum unless block_given?
-    
-        while to_a.length > a
-          yield(to_a[a])
-          a += 1
-        end
-        self
-      end
+  def my_each
+    a = 0
+    return enum unless block_given?
+
+    while a.length < a
+      yield(a[a])
+      a += 1
+    end
+    self
   end
+end
