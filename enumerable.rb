29687e92c7a101d63838e2a53c6d4cdef4407eca
verissimohenry <verissimohenry04@gmail.com>
2021-04-05 08:12:52 +0100

    my any solution

--- enumerable.rb
+++ enumerable.rb
@@ -33,3 +33,45 @@ def my_select
   new_array
 end
 ["Brian", "Susan", "Mandy", "John"].my_each {|friend| puts friend }
+
+
+def my_all?(params = nil)
+  if block_given?
+    to_a.my_each { |item| return false if yield(item) == false }
+  elsif params.nil?
+    to_a.my_each { |item| return false if item.nil? || item == false }
+  elsif !params.nil? && (params.is_a? Class)
+    to_a.my_each { |item| return false unless [item.class, item.class.superclass].include?(params) }
+  elsif !params.nil? && params.instance_of?(Regexp)
+    to_a.my_each { |item| return false unless item.match(params) }
+  else
+    to_a.my_each { |item| return false if item != params }
+  end
+  true
+end
+["Brian", "Susan", "Mandy", "John"].my_each {|friend| puts friend }
+
+def my_any?(params = nil)
+  if block_given?
+    to_a.my_each { |item| return true if yield(item) }
+    return false
+  elsif params.nil?
+    to_a.my_each { |item| return true if item }
+  elsif !params.nil? && (params.is_a? Class)
+    to_a.my_each { |item| return true if [item.class, item.class.superclass].include?(params) }
+  elsif !params.nil? && params.instance_of?(Regexp)
+    to_a.my_each { |item| return true if item.match(params) }
+  else
+    to_a.my_each { |item| return true if item == params }
+  end
+  false
+end
+["Brian", "Susan", "Mandy", "John"].my_each {|friend| puts friend }
+
+def my_none?(prms = nil)
+  if block_given?
+    !my_any?(&Proc.new)
+  else
+    !my_any?(prms)
+  end
+end
