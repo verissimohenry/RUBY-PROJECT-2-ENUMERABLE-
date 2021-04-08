d80d38950fc44b41d3ba2c5bb1839acf0bee8a97
verissimohenry <verissimohenry04@gmail.com>
2021-04-08 16:26:28 +0100

    add my map test

--- spec/enumerables_test.rb
+++ spec/enumerables_test.rb
@@ -131,4 +131,60 @@ describe '#my_all' do
       expect([1, 2, 3].my_each_with_index.instance_of?(Enumerator)).to eql(true)
     end
   end
+
+  #   my_inject tests
+
+  describe '#my_inject' do
+    let(:search) { proc { |memo, word| memo.length > word.length ? memo : word } }
+    let(:words) { %w[dog door rod blade] }
+    let(:array) { Array.new(100) { rand(0...9) } }
+    let(:range) { Range.new(5, 50) }
+
+    it 'search for the longest word in array of strings' do
+      expect(words.my_inject(&search) == words.inject(&search)).to eql(true)
+    end
+
+    it 'check if original array is not mutated ' do
+      array_clone = array.clone
+      array.my_inject { |num| num + 1 }
+      expect(array == array_clone).to eql(true)
+    end
+
+    it 'when a block is given without initial value it combines all elements of enum, apply binary spc by block' do
+      operation = proc { |sum, n| sum + n }
+      expect(array.my_inject(&operation) == array.inject(&operation)).to eql(true)
+    end
+
+    it 'when a block is given without initial value it combines all elements of enum, apply binary spc by block:range' do
+      actual = range.my_inject { |prod, n| prod * n }
+      expected = range.inject { |prod, n| prod * n }
+      expect(actual == expected).to eql(true)
+    end
+
+    it 'when a symbol is given without initial value it combines all elements of enum, apply binary spc b named method' do
+      expect(array.my_inject(:+) == array.inject(:+)).to eql(true)
+    end
+
+    it 'when a symbol is given without initial value it combines all elements of enum, apply binary spc b named method' do
+      expect(array.my_inject(:*) == array.inject(:*)).to eql(true)
+    end
+
+    it ' when a sym is specified with an initial value, sym as named method ' do
+      expect(array.my_inject(20, :*) == array.inject(20, :*)).to eql(true)
+    end
+
+    it ' when a sym is specified with an initial value: sym as named method range ' do
+      expect(range.my_inject(2, :*) == range.inject(2, :*)).to eql(true)
+    end
+  end
+
+  #   #my_map tests
+  describe '#my_map' do
+    let(:my_proc) { proc { |num| num > 10 } }
+    let(:array) { Array.new(100) { rand(0...9) } }
+
+    it 'executes only the proc when the block and proc is given' do
+      expect(array.my_map(&my_proc) == array.map(&my_proc)).to eql(true)
+    end
+  end
 end
