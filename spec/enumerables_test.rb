1aa67f53a9b3c779511a6ce1c9b6b43747a24f91
verissimohenry <verissimohenry04@gmail.com>
2021-04-08 15:52:39 +0100

    add test

--- spec/enumerables_test.rb
+++ spec/enumerables_test.rb
@@ -12,5 +12,46 @@ describe '#my_all' do
   it 'should return true if block never returns false or nil' do
     expect(array.my_all?(&true_block) == array.all?(&true_block)).to eql(true)
   end
+
+  it 'should return true if block returns false or nil' do
+    expect(array.my_all?(&false_block) == array.all?(&false_block)).to eql(true)
+  end
+
+  it 'should return true if block returns false or nil:range' do
+    expect(range.my_all?(&false_block) == range.all?(&false_block)).to eql(true)
+  end
+
+  it 'should return true if all elements of an array are true' do
+    expect(true_array.my_all? == true_array.all?).to eql(true)
+  end
+
+  it 'should return true if any of elements of an array are false' do
+    expect(false_array.my_all? == false_array.all?).to eql(true)
+  end
+
+  it 'must return true when regex is passed as an argument all of the collection matches the regex' do
+    expect(words.my_all?(/d/) == words.all?(/d/)).to eql(true)
+  end
+
+  it 'must return true when regex is passed as an argument all of the collection does not match the regex' do
+    expect(words.my_all?(/o/) == words.all?(/o/)).to eql(true)
+  end
+
+  it 'pattern other than regex or class given return true if all collection matches the pattern of local var' do
+    array = []
+    10.times { array << 3 }
+    expect(array.my_all?(3) == array.all?(3)).to eql(true)
+  end
+
+  it 'pattern other than regex or class given return true if all collection matches the pattern of global var' do
+    expect(array.my_all?(3) == array.all?(3)).to eql(true)
+  end
+
+  it 'should return true if all elements are of Integer type' do
+    expect(array.my_all?(Integer) == array.all?(Integer)).to eql(true)
+  end
+
+  it 'should return true if all elements are of Numeric type' do
+    expect(array.my_all?(Numeric) == array.all?(Numeric)).to eql(true)
+  end
 end
-  
